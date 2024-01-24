f48b04a3bf59bc87e19270fb3e6332c1337726cc
Dylan Gallagher <gallagdy@tcd.ie>
2024-01-24 08:06:34 +0000

    Added plotting functionality
    Used fastcore and matplotlib to add plotting functionality

--- my_miniai/dataset.py
+++ my_miniai/dataset.py
@@ -44,3 +44,52 @@ class BatchSampler:
     def __iter__(self):
         yield from fc.chunked(iter(self.sampler), self.bs, drop_last=self.drop_last)
 
+
+@fc.delegates(plt.subplots, keep=True)
+def subplots(
+    nrows:int=1, # Number of rows in returned axes grid
+    ncols:int=1, # Number of columns in returned axes grid
+    figsize:tuple=None, # Width, height in inches of the returned figure
+    imsize:int=3, # Size (in inches) of images that will be displayed in the returned figure
+    suptitle:str=None, # Title to be set to returned figure
+    **kwargs
+): # fig and axs
+    "A figure and set of subplots to display images of `imsize` inches"
+    if figsize is None: figsize=(ncols*imsize, nrows*imsize)
+    fig,ax = plt.subplots(nrows, ncols, figsize=figsize, **kwargs)
+    if suptitle is not None: fig.suptitle(suptitle)
+    if nrows*ncols==1: ax = np.array([ax])
+    return fig,ax
+
+
+@fc.delegates(subplots)
+def get_grid(
+    n:int, # Number of axes
+    nrows:int=None, # Number of rows, defaulting to `int(math.sqrt(n))`
+    ncols:int=None, # Number of columns, defaulting to `ceil(n/rows)`
+    title:str=None, # If passed, title set to the figure
+    weight:str='bold', # Title font weight
+    size:int=14, # Title font size
+    **kwargs,
+): # fig and axs
+    "Return a grid of `n` axes, `rows` by `cols`"
+    if nrows: ncols = ncols or int(np.floor(n/nrows))
+    elif ncols: nrows = nrows or int(np.ceil(n/ncols))
+    else:
+        nrows = int(math.sqrt(n))
+        ncols = int(np.floor(n/nrows))
+    fig,axs = subplots(nrows, ncols, **kwargs)
+    for i in range(n, nrows*ncols): axs.flat[i].set_axis_off()
+    if title is not None: fig.suptitle(title, weight=weight, size=size)
+    return fig,axs
+
+
+@fc.delegates(subplots)
+def show_images(ims:list, # Images to show
+                nrows:int|None=None, # Number of rows in grid
+                ncols:int|None=None, # Number of columns in grid (auto-calculated if None)
+                titles:list|None=None, # Optional list of titles for each image
+                **kwargs):
+    "Show all images `ims` as subplots with `rows` using `titles`"
+    axs = get_grid(len(ims), nrows, ncols, **kwargs)[1].flat
+    for im,t,ax in zip_longest(ims, titles or [], axs): show_image(im, ax=ax, title=t)
