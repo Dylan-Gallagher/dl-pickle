69f78a675a722663cd01911fb79107199404df1b
Dylan Gallagher <gallagdy@tcd.ie>
2024-01-24 07:58:15 +0000

    Refactored minibatch.py
    Transfered code from notebook to .py file

--- my_miniai/minibatch.py
+++ my_miniai/minibatch.py
@@ -1,5 +1,8 @@
-import math
-import torch
+import pickle, gzip, math, os, time, shutil, torch, matplotlib as mpl, numpy as np, matplotlib.pyplot as plt
+from pathlib import Path
+from torch import tensor, nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, SequentialSampler, RandomSampler, BatchSampler
 
 
 def log_sum_exp(x):
@@ -18,6 +21,44 @@ def accuracy(out, yb):
 def report(loss, preds, yb):
     print(f'{loss:.2f}, {accuracy(preds, yb):.2f}')
 
+
+class Dataset:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, item):
+        return self.x[item], self.y[item]
+
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
+    for epoch in range(epochs):
+        model.train()
+        for xb,yb in train_dl:
+            loss = loss_func(model(xb), yb)
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+
+        model.eval()
+        with torch.no_grad():
+            tot_loss,tot_acc,count = 0.,0.,0
+            for xb,yb in valid_dl:
+                pred = model(xb)
+                n = len(xb)
+                count += n
+                tot_loss += loss_func(pred,yb).item()*n
+                tot_acc  += accuracy (pred,yb).item()*n
+        print(epoch, tot_loss/count, tot_acc/count)
+    return tot_loss/count, tot_acc/count
+
+
+def get_dls(train_ds, valid_ds, bs, **kwargs):
+    return (DataLoader(train_ds, batch_size=bs, shuffle=True, **kwargs),
+            DataLoader(valid_ds, batch_size=bs*2, **kwargs))
 
 
 
