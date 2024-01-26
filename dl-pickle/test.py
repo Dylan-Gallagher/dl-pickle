4348c01e1d220f2ff03ddc299a3012b01acb1fe1
Dylan Gallagher <gallagdy@tcd.ie>
2024-01-26 18:25:24 +0000

    Moved test.py to root directory

--- dl-pickle/test.py
+++
@@ -1,3 +0,0 @@
-import imports
-
-r = Relu()
---
+++ test.py
@@ -0,0 +1,9 @@
+from learner import *
+
+m = 28 * 28
+nh = 50
+
+
+def get_model():
+    return nn.Sequential(nn.Linear(m, nh), nn.ReLU(), nn.Linear(nh, 10))
+
